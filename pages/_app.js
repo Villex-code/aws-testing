00184f8cc414f4741ef914b24b7f3babb21a07bb
Villex <videlik@gmail.com>
2023-02-14 20:47:33 +0200

    please god

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,9 @@ import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export default function App({ Component, pageProps }) {
+  const { user } = pageProps;
   return (
-    <UserProvider>
+    <UserProvider user={user}>
       <Component {...pageProps} />
     </UserProvider>
   );
--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,19 +1,5 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+// pages/api/auth/[...auth0].js
+import auth0 from "../../../utils/auth0";
 
-console.log("I am in the entry file");
-console.log("the AUTH0_SECRET env var is set: ", process.env.AUTH0_SECRET);
-console.log("the AUTH0_BASE_URL env var is set: ", process.env.AUTH0_BASE_URL);
-console.log(
-  "the AUTH0_ISSUER_BASE_URL env var is set: ",
-  process.env.AUTH0_ISSUER_BASE_URL
-);
-console.log(
-  "the AUTH0_CLIENT_SECRET env var is set: ",
-  process.env.AUTH0_CLIENT_SECRET
-);
-console.log(
-  "the AUTH0_CLIENT_ID env var is set: ",
-  process.env.AUTH0_CLIENT_ID
-);
-
-export default handleAuth();
+// Use the instance method
+export default auth0.handleAuth();
