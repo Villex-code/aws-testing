c13c989d63735bd9c437e37998e73c6ce2e3de65
Villex <videlik@gmail.com>
2023-02-14 22:20:22 +0200

    env checker

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home({ data }) {
           <p>{user.email}</p>
         </div>
       )} */}
-      New deployment
+      New deployment ayo
       <br />
       <Link href="/api/auth/login">Login</Link>
       <br />
